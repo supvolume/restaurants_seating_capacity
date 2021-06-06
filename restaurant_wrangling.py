f01887d84e20e7ba923b49f8a19d8d0d9c76089d
supvolume <h.supida@gmail.com>
2021-06-06 21:01:46 +1000

    separate the latest year
    - Select only data in 2019 to output as restaurant location for display in the first tab of R Shiny
    - Reorder the code to reduce confusion

--- restaurant_wrangling.py
+++ restaurant_wrangling.py
@@ -7,14 +7,14 @@ import pandas as pd
 
 # Function to create the description
 def get_desc(df):
-    description = '<b><ins>' + df['Trading name'] + '</ins></b><br>' + \
-                  df['Street address']
+    description = '<b><ins>' + df['name'] + '</ins></b><br>' + \
+                  df['address'] + '<br>'
     # Add number of indoor seating
-    if not pd.isnull(df['Seats - Indoor']):
-        description += '<br><br><b>Indoor seats:</b>' + str(df['Seats - Indoor'])
+    if not pd.isnull(df['seat_indoor']):
+        description += '<br><b>Indoor seats:</b>' + str(int(df['seat_indoor']))
     # Add number of outdoor seating
-    if not pd.isnull(df['Seats - Outdoor']):
-        description += '<br><br><b>Outdoor seats:</b>' + str(df['Seats - Outdoor'])
+    if not pd.isnull(df['seat_outdoor']):
+        description += '<br><b>Outdoor seats:</b>' + str(int(df['seat_outdoor']))
     return description
 
 
@@ -23,54 +23,57 @@ df = pd.read_csv('Cafes_and_restaurants__with_seating_capacity.csv')
 
 # Select related columns
 df = df[['Census year',
-          'Block ID',
-          'Property ID',
-          'Base property ID',
-          'Street address',
-          'CLUE small area',
-          'Trading name',
-          'Industry (ANZSIC4) description',
-          'Seating type',
-          'Number of seats',
-          'x coordinate',
-          'y coordinate']]
+         'Street address',
+         'CLUE small area',
+         'Trading name',
+         'Industry (ANZSIC4) description',
+         'Seating type',
+         'Number of seats',
+         'x coordinate',
+         'y coordinate']]
 
 # Drop the null row
 df.dropna()
 
-# Select only the industry types that are related to cafe and restaurants
-res_related = ['Cafes and Restaurants',
-               'Pubs, Taverns and Bars',
-               'Takeaway Food Services']
-res_df = df[df['Industry (ANZSIC4) description'].isin(res_related)]
-
-# Spread seating type into two columns
-seat_df = res_df.pivot_table(index=['Census year',
-                                    'Block ID',
-                                    'Property ID',
-                                    'Base property ID',
-                                    'Street address',
-                                    'CLUE small area',
-                                    'Trading name',
-                                    'Industry (ANZSIC4) description',
-                                    'x coordinate',
-                                    'y coordinate'],
-                             columns='Seating type',
-                             values='Number of seats').reset_index()
-
-# Get description
-seat_df["Description"] = seat_df.apply(get_desc, axis=1)
-
-# Rename column for easier management in R
-seat_df = seat_df.rename(columns={'Census year':'year',
+# Rename column for easier management
+df = df.rename(columns={'Census year':'year',
+                        'Street address': 'address',
                         'CLUE small area': 'suburb',
                         'Trading name': 'name',
                         'Industry (ANZSIC4) description': 'res_type',
                         'x coordinate': 'lon',
                         'y coordinate': 'lat',
-                        'Seats - Indoor': 'seat_indoor',
-                        'Seats - Outdoor': 'sear_outdoor',
                         'Number of seats': 'num_of_seats'})
 
-# TODO: If R error, replace Null with 0
-seat_df.to_csv('clean_restaurants_seats.csv')
+# Select only the industry types that are related to cafe and restaurants
+res_related = ['Cafes and Restaurants',
+               'Pubs, Taverns and Bars',
+               'Takeaway Food Services']
+res_df = df[df['res_type'].isin(res_related)]
+
+# Spread seating type into two columns
+seat_df = res_df.pivot_table(index=['year',
+                                    'address',
+                                    'suburb',
+                                    'name',
+                                    'res_type',
+                                    'lon',
+                                    'lat'],
+                             columns='Seating type',
+                             values='num_of_seats').reset_index()
+
+# Rename seating type columns
+seat_df = seat_df.rename(columns={
+                    'Seats - Indoor': 'seat_indoor',
+                    'Seats - Outdoor': 'seat_outdoor'})
+
+
+# For display the current restaurant in the City of Melbourne (2019)
+seat_2019 = seat_df[seat_df['year'] == 2019].copy()
+
+# Get description
+seat_2019["description"] = seat_2019.apply(get_desc, axis=1)
+seat_2019.reset_index(inplace=True, drop=True)
+
+# Output location data in CSV file
+seat_2019.to_csv('clean_restaurants_seats.csv', index=False)
